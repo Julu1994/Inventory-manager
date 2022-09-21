99abac11436e99592b143dff56a0c26faf41b387
julu1994 <mrjewel837@gmail.com>
2022-09-21 12:44:57 +0200

    added mui header

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/header";
+import Homepage from "./Pages/homepage";
+import AddProduct from "./Pages/addProduct";
 function App() {
-    return <div className="App">Hello World</div>;
+    return (
+        <div className="App">
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/login" element={<AddProduct />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
