855ed270ed8cbaada85f7ee8e916ecca1d8f13a8
Julu1994 <mrjewel837@gmail.com>
2023-06-07 15:17:13 +0200

    added mongodb session to protect collections from inconsistent transection

--- server/controllers/products.controler.js
+++ server/controllers/products.controler.js
@@ -53,13 +53,17 @@ export const addNewProducts = async (req, res) => {
 };
 
 export const removeProducts = async (req, res) => {
+  const session = await mongoose.startSession();
+
   try {
+    session.startTransaction();
+
     const productID = req.params.id;
     if (!productID)
       return res.status(400).json({
         error: 'Nothing to delete!',
       });
-    const existedProduct = await ProductModel.findById(productID);
+    const existedProduct = await ProductModel.findById(productID).session(session);
     if (!existedProduct)
       return res.status(400).json({
         error: 'Sorry!!! not found anything to delete....',
@@ -73,30 +77,37 @@ export const removeProducts = async (req, res) => {
       product: productID,
       change: -existedProduct.quantity,
     });
-    await quantityChange.save();
+    await quantityChange.save({ session });
 
-    await ProductModel.findByIdAndDelete(productID);
+    await ProductModel.findByIdAndDelete(productID).session(session);
 
     const removedProduct = new RemovedProductModel({
       productId: productID,
       removedAt: new Date()
     });
-    await removedProduct.save();
+    await removedProduct.save({ session });
+
+    await session.commitTransaction();
 
     return res.status(200).json({
       message: 'Product deleted successfully',
     });
   } catch (err) {
+    await session.abortTransaction();
     console.log(err);
     return res.status(500).json({
       error: 'Something went wrong',
     });
+  } finally {
+    session.endSession();
   }
 };
 
-
 export const editProducts = async (req, res) => {
+  const session = await mongoose.startSession();
   try {
+    session.startTransaction();
+
     const { name, details, price, quantity, location, catagory, type, url } =
       req.body;
     const productID = req.params.id;
@@ -111,7 +122,7 @@ export const editProducts = async (req, res) => {
         error: 'Error! Something went wrong',
       });
 
-    const existingProduct = await ProductModel.findById(productID);
+    const existingProduct = await ProductModel.findById(productID).session(session);
 
     if (!existingProduct)
       return res.status(400).json({
@@ -129,7 +140,7 @@ export const editProducts = async (req, res) => {
       product: productID,
       change: quantityDifference
     });
-    await quantityChange.save();
+    await quantityChange.save({ session });
 
     existingProduct.name = name;
     existingProduct.details = details;
@@ -140,16 +151,24 @@ export const editProducts = async (req, res) => {
     existingProduct.type = type;
     existingProduct.url = url;
 
-    const savedProduct = await existingProduct.save();
+    const savedProduct = await existingProduct.save({ session });
+
+    await session.commitTransaction();
+
     res.json(savedProduct);
   } catch (error) {
-    res.status(500).json({ errore: 'Error!! Something went worong' });
+    await session.abortTransaction();
+    res.status(500).json({ error: 'Error!! Something went wrong' });
+  } finally {
+    session.endSession();
   }
 };
 
-
 export const shrinkProducts = async (req, res) => {
+  const session = await mongoose.startSession();
   try {
+    session.startTransaction();
+
     const { quantity } = req.body;
     const productID = req.params.id;
     if (!quantity) {
@@ -162,7 +181,7 @@ export const shrinkProducts = async (req, res) => {
         error: 'Error! Something went wrong',
       });
 
-    const existingProduct = await ProductModel.findById(productID);
+    const existingProduct = await ProductModel.findById(productID).session(session);
 
     if (!existingProduct)
       return res.status(400).json({
@@ -178,21 +197,30 @@ export const shrinkProducts = async (req, res) => {
     const newQuantity = parseInt(existingProduct.quantity) - parseInt(quantity);
 
     existingProduct.quantity = newQuantity;
-    const savedProduct = await existingProduct.save();
+    const savedProduct = await existingProduct.save({ session });
     const quantityChange = new QuantityChangeModel({
       product: productID,
       change: newQuantity - oldQuantity,
     });
 
-    await quantityChange.save();
+    await quantityChange.save({ session });
+
+    await session.commitTransaction();
 
     res.json(savedProduct);
   } catch (error) {
+    await session.abortTransaction();
     res.status(500).json({ error: 'Error!! Something went wrong' });
+  } finally {
+    session.endSession();
   }
 };
+
 export const inboundProducts = async (req, res) => {
+  const session = await mongoose.startSession();
   try {
+    session.startTransaction();
+
     const { quantity } = req.body;
     const productID = req.params.id;
     if (!quantity) {
@@ -205,7 +233,7 @@ export const inboundProducts = async (req, res) => {
         error: 'Error! Something went wrong',
       });
 
-    const existingProduct = await ProductModel.findById(productID);
+    const existingProduct = await ProductModel.findById(productID).session(session);
 
     if (!existingProduct)
       return res.status(400).json({
@@ -220,16 +248,22 @@ export const inboundProducts = async (req, res) => {
     const oldQuantity = existingProduct.quantity;
     const newQuantity = parseInt(existingProduct.quantity) + parseInt(quantity);
     existingProduct.quantity = newQuantity;
-    const savedProduct = await existingProduct.save();
+    const savedProduct = await existingProduct.save({ session });
 
     const quantityChange = new QuantityChangeModel({
       product: productID,
       change: newQuantity - oldQuantity,
     });
 
-    await quantityChange.save();
+    await quantityChange.save({ session });
+
+    await session.commitTransaction();
+
     res.json(savedProduct);
   } catch (error) {
-    res.status(500).json({ errore: 'Error!! Something went worong' });
+    await session.abortTransaction();
+    res.status(500).json({ error: 'Error!! Something went wrong' });
+  } finally {
+    session.endSession();
   }
 };
