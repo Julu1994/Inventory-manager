9abadc13cbcbc034155978ce5423fee4f2e3ba15
Julu1994 <mrjewel837@gmail.com>
2023-06-07 15:23:07 +0200

    modified the user user schema implimenting indexing and some other best prectices

--- server/models/auth.model.js
+++ server/models/auth.model.js
@@ -1,13 +1,30 @@
 import mongoose from 'mongoose';
-const userScehema = new mongoose.Schema(
+
+const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    H_password: { type: String, required: true },
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true
+    },
+    H_password: {
+      type: String,
+      required: true
+    },
   },
   {
     timestamps: true,
   }
 );
+userSchema.methods.toJSON = function () {
+  var obj = this.toObject();
+  delete obj.H_password;
+  return obj;
+}
 
-export const User = mongoose.model('user', userScehema);
+export const User = mongoose.model('user', userSchema);
