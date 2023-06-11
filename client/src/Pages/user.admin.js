438d3f9223b0f6254a6be4c8f0d463a1596ebcee
Julu1994 <mrjewel837@gmail.com>
2023-06-11 23:46:58 +0200

    working on adding new user ui

--- client/src/Pages/user.admin.js
+++ client/src/Pages/user.admin.js
@@ -1,5 +1,5 @@
 
-import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
+import { Button, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import React from "react";
@@ -16,32 +16,49 @@ const AdminUser = () => {
   return (
     <div>
       <Box>
-        <TextField
-          id="filled-search"
-          label="Email"
-          type="searce"
-          variant="filled"
-        />
-        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
+        <Grid container spacing={2} style={{
+          minHeight: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+          <Grid item xs={12} sm={12} md={4}>
+            <Box>
+              <TextField
+                sx={{ width: '100%' }}
+                id="outlined-multiline-flexible"
+                label="Email"
+                multiline
+                maxRows={4}
+              />
+            </Box>
+            <Box>
+              <FormControl sx={{ width: '100%' }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+            </Box>
+            <Box>
+              <Button sx={{ width: "100%" }} variant="contained"> Add User</Button>
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
     </div>
 
